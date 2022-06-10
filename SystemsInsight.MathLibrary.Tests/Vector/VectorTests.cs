37383ce37ace98bd4d91123e9ee52a01f7d1619e
Systems Insight <systemsinsight@outlook.com>
2022-06-10 18:01:49 +0100

    Added test.

--- SystemsInsight.MathLibrary.Tests/Vector/VectorTests.cs
+++ SystemsInsight.MathLibrary.Tests/Vector/VectorTests.cs
@@ -7,6 +7,24 @@ namespace SystemsInsight.MathLibrary.Tests
         public void Operator_Add_Int_And_Vector_Expect_Success()
         {
             // Arrange:
+            var x = 10.0;
+            var y = new Vector(3.5, 1.1, 8.9);
+
+            var expectedResult = new Vector(13.5, 11.1, 18.9);
+
+            // Act:
+            var result = x + y;
+
+            var isEqual = result == expectedResult;
+
+            // Assert:
+            Assert.IsTrue(isEqual);
+        }
+
+        [TestMethod]
+        public void Operator_Add_Vector_And_Vector_Expect_Success()
+        {
+            // Arrange:
             var x = new Vector(1.0, -2.1, 6.5);
             var y = new Vector(3.5,  1.1, 8.9);
 
